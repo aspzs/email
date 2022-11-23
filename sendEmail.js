eadeaad404fa3a85c6a3f6296291f35b816cb5e4
aspzs <25sparnol@gmail.com>
2022-11-22 22:48:03 -0600

    This script have an html template, need to change some values, but works

--- sendEmail.js
+++ sendEmail.js
@@ -1,96 +1,44 @@
 //npm install nodemailer para activarlo
+//npm install nodemailer-express-handlebars -S
 //Dominio http://localhost:4242
-
-
-const nodemailer = require("nodemailer");
-const env = require('dotenv');
-
 //Contiene infomracion del mail prinncipal
 
 
-// nodemailer.createTestAccount((err, account) => {
-//     if(err) {
-//         console.log('Failed');
-//         console.log(err);
-//         return process.exit(1);
-//     }
+const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-handlebars');
+const log = console.log;
 
-//     console.log('Sending message');
-// })
 
-nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    //587 para no tener secure
+// We made the transporter
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
     port: 465,
-    secure: true, // upgrade later with STARTTLS
+    secure: true,
     auth: {
-      user: 'process.env.SMTP_USERNAME',
-      pass: 'process.env.SMTP_PASSWORD',
+        user: '25sparnol@gmail.com',  
+        pass: 'evufodyhefjnkdwu', 
     },
-  });
-
-
-
-
-const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'process.env.SMTP_USERNAME',
-            pass: 'process.env.SMTP_PASSWORD',
-          },
-     });
-
-const mailOption = {
-    from: 'martqetplace@gmail.com',
-    //Aqui se cambia el mail del usuario
-    //Este mail es de testing
-    to: '25sparnol@gmail.com',
-    subject: 'EMAIL TEST',
-    text: 'Este email es de test'
-};
-
-transporter.sendMail(mailOption, function(error, info){
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Ya se mandó el mail' + info.response);
-    }
 });
 
 
-// let message = {
-//     from: '<>',
-//     to: '<25sparnol@gmail.com>',
-//     subject: 'EMAIL Test',
-//     text: 'Esto es un test',
-//     html: `<p><b>TEST</b> <img src="https://images.unsplash.com/photo-1668603486663-5fd96b778391?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"></p>
-//     <p>Esto es un test de la pag de html<br/></p>`,
-//     amp: `<!DOCTYPE html>
-//     <html ⚡4email data-css-strict>
-//       <head>
-//         <meta charset="utf-8" />
-//         <style amp4email-boilerplate>
-//           body {
-//             visibility: hidden;
-//           }
-//         </style>
-//         <script async src="https://cdn.ampproject.org/v0.js"></script>
-//         <script async custom-element="amp-anim" src="https://cdn.ampproject.org/v0/amp-anim-0.1.js"></script>
+transporter.use('compile', hbs({
+    viewEngine: 'express-handlebars',
+    viewPath: './views/'
+}));
 
-//       </head>
-//       <body>
-//         Hello, this is a test.
-//       </body>
-//     </html>`
-// };
 
-// transporter.sendMail(message, (error, info) => {
-//     if(err) {
-//         console.log('Error en el test');
-//         return process.exit(1);
-//     }
+let mailOptions = {
+    from: '25sparnol@gmail.com', // TODO: email sender
+    to: '25sparnol@gmail.com', // TODO: email receiver
+    subject: 'Thank you for shopping with MANDALA',
+    text: 'Hola hola',
+    template: 'index',
+};
 
-//     console.log('El mensaje se mandó bien');
-//     console.log(nodemailer.getTestMessageUrl(info));
-//     transporter.close();
-// })
+// We use the .sendMail() function that receives two values
+transporter.sendMail(mailOptions, (err, data) => {
+    if (err) {
+        return log('Error occurs');
+    }
+    return log('Email Enviado! 👍📧');
+});
